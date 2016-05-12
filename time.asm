971ced4326c0d8377f017a2a0fd87889b83d55a7
PCYoshi <josephcaillet@gmail.com>
2016-05-12 17:32:45 +0200

    time

--- time.asm
+++ time.asm
@@ -19,7 +19,12 @@ ST7/
 ;
 ;************************************************************************
 	
+	PUBLIC wait1ms
+	PUBLIC wait2ms
+	PUBLIC wait500ms
+	PUBLIC wait
 	
+	PUBLIC waitTime
 
 ;************************************************************************
 ;
@@ -28,6 +33,8 @@ ST7/
 ;************************************************************************
 	BYTES
 	segment byte 'ram0'
+
+waitTime DS.B 1
 
 ;************************************************************************
 ;
@@ -44,6 +51,75 @@ ST7/
 ;************************************************************************
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;
+; temporisation de 1ms ;
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+wait1ms:
+	;Nloop_1ms	EQU	98	; nombre de boucle pour tempo de 1ms = 98 x Fcpu en MHz
+	; 98 si fcpu = 1MHz; 74 si fcpu = 0,76MHz; 
+	PUSH	X			; 3 cycles
+	; nombre de boucle = Nloop_1ms
+	LD		X,#98		; 2 cycles      
+tempo10cyc:
+	; boucle 10 cycles
+	NOP					; 2 cycles
+	NOP					; 2 cycles
+	DEC 	X			; 3 cycles X = X-1
+	JRNE	tempo10cyc ; 3 cycles si X <> 0 alors continue tempo
+	POP		X			; 4 cycles
+	RET ;= 6 cycles
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+; temporisation de 2ms ;
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+wait2ms:
+	;Nloop_2ms	EQU	198	; nombre de boucle pour tempo de 2ms = 198 x Fcpu en MHz
+	; 198 si fcpu = 1MHz; 150 si fcpu = 0,76MHz; 
+	PUSH	X			; 3 cycles
+	; nombre de boucle = Nloop_2ms
+	LD		X,#198; 	2 cycles      
+tempo10cyc2:
+	; boucle 10 cycles
+	NOP					; 2 cycles
+	NOP					; 2 cycles
+	DEC 	X			; 3 cycles X = X-1
+	JRNE	tempo10cyc2 ; 3 cycles si X <> 0 alors continue tempo
+	POP		X			; 4 cycles
+	RET ;= 6 cycles
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+; temporisation de 500ms ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+wait500ms:
+	PUSH X
+	LD X,250
+boucle_250_wait500ms:
+	CALL wait2ms
+	DEC X
+	JRNE boucle_250_wait500ms
+	POP X
+	RET
+	
+	
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; temporisation de waitTime ms ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+wait:
+	PUSH X
+	LD X,waitTime
+boucle_waitTime_wait500ms:
+	CALL wait1ms
+	DEC X
+	JRNE boucle_waitTime_wait500ms
+	POP X
+	RET
 
 
 ;************************************************************************
