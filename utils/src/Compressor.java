b2f961412cb89207279ed03f4406021e98001670
PCYoshi <josephcaillet@gmail.com>
2016-05-18 18:10:10 +0200

    adding compressor utils

--- utils/src/Compressor.java
+++ utils/src/Compressor.java
@@ -35,6 +35,7 @@ public class Compressor
 
 		for(int y = 0; y < img.getHeight(); y++)
 		{
+			System.out.println("\nLine : " + y);
 			Color lastColor = new Color(img.getRGB(0, y));
 			int repetition = 0;
 
@@ -49,7 +50,8 @@ public class Compressor
 				}
 				else
 				{
-					//System.out.println("Line " + y + " : " + x + " \tOld : " + lastColor + "\tNew : " + newColor);
+					repetition += addColorBit(lastColor, c1, c2, c3, c4);
+
 					result += repetition + ", ";
 
 					repetition = 0;
@@ -57,9 +59,35 @@ public class Compressor
 				}
 			}
 
+			repetition += addColorBit(lastColor, c1, c2, c3, c4);
 			result += repetition + "\n";
 		}
 
 		return result;
+	}
+
+	private static int addColorBit(Color current, Color c1, Color c2, Color c3, Color c4)
+	{
+		if(current.equals(c1))
+		{
+			System.out.println("Noir");
+			return Integer.parseInt("00000000",2);
+		}
+		else if(current.equals(c2))
+		{
+			System.out.println("Gris");
+			return Integer.parseInt("01000000",2);
+		}
+		else if(current.equals(c3))
+		{
+			System.out.println("Blanc");
+			return Integer.parseInt("10000000",2);
+		}
+		else if(current.equals(c4))
+		{
+			System.out.println("Rouge");
+			return Integer.parseInt("11000000",2);
+		}
+		return -255;
 	}
 }
