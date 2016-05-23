7371bc38fe23c20fea8bd868f8c8f37431a9914f
Nephylhim <thomas.coussot@laposte.net>
2016-05-23 15:11:20 +0200

    prepare game

--- main.asm
+++ main.asm
@@ -48,7 +48,7 @@ test DS.B 1
 ;
 ;************************************************************************
 
-
+ship_state DS.B 1
 
 
 ;************************************************************************
@@ -85,7 +85,122 @@ test DS.B 1
 ;
 ;************************************************************************
 
+;----------------------------------------------------;
+;-                       init                       -;
+;----------------------------------------------------;
+init:
+	RSP			; Reset Stack Pointer
+	CALL	initPortSPI
+	CALL	initOsc
+	CALL	initTFT
 
+	call init_masks
+	call init_game
+
+	ret
+
+
+;----------------------------------------------------;
+;-                    init masks                    -;
+;----------------------------------------------------;
+init_masks:
+	ld a,paddr
+	and a,#%11110111
+	ld paddr,a
+
+	ld a,paor
+	or a,#%00001000
+	ld paor,a
+
+	ld a,pbddr
+	and a,#%11111110
+	ld pbddr,a
+
+	ld a,pbor
+	or a,#%00000001
+	ld pbor,a
+
+	ld a,eicr
+	;and a,#%10111110
+	or a,#%11000011
+	ld eicr,a
+
+	ld a,eisr
+	and a,#%11111100
+	or a,#%11000000
+	ld eisr,a
+
+	ret
+
+
+;----------------------------------------------------;
+;-                    init game                     -;
+;----------------------------------------------------;
+init_game:
+	push a
+
+	LD	A,#$00
+	LD	colorMSB,A
+	LD	A,#$00
+	LD	colorLSB,A
+	LD	A,#0
+	LD	x0win,A
+	LD	y0win,A
+	LD	A,#128
+	LD	width,A
+	LD	A,#160
+	LD	height,A
+	CALL	fillRectTFT
+
+	pop a
+	ret
+
+
+;----------------------------------------------------;
+;-               display title screen               -;
+;----------------------------------------------------;
+display_title_screen:
+	push a
+
+	LD	A,#20
+	LD	numSprite,A
+	CALL	setSprite
+	LD	A,#2
+	LD	dspCoef,A
+	LD	A,#0
+	LD	dsp0X,A
+	LD	dsp0Y,A
+	CALL	dspSprite
+	
+	LD	A,#22
+	LD	numSprite,A
+	CALL	setSprite
+	LD	A,#2
+	LD	dspCoef,A
+	LD	A,#40
+	LD	dsp0Y,A
+	CALL	dspSprite
+	
+	LD	A,#24
+	LD	numSprite,A
+	CALL	setSprite
+	LD	A,#2
+	LD	dspCoef,A
+	LD	A,#80
+	LD	dsp0Y,A
+	CALL	dspSprite
+	
+	LD	A,#26
+	LD	numSprite,A
+	CALL	setSprite
+	LD	A,#2
+	LD	dspCoef,A
+	LD	A,#120
+	LD	dsp0Y,A
+	CALL	dspSprite
+
+	pop a
+	ret
 
 
 
@@ -103,10 +218,10 @@ test DS.B 1
 ;************************************************************************
 
 main:
-	RSP			; Reset Stack Pointer
-	CALL	initPortSPI
-	CALL	initOsc
-	CALL	initTFT
+	call init
+
+	call display_title_screen
+
 boucl
 
 	
@@ -123,6 +238,23 @@ boucl
 
 dummy_rt:	IRET	; Procédure vide : retour au programme principal.
 
+ship_forward:
+	BTJT	PBDR,#0,pb0_push
+	clr ship_state
+	iret
+pb0_push
+	ld a,#1
+	ld ship_state,a
+	iret
+
+ship_backward:
+	BTJT	PADR,#3,pa3_push
+	clr ship_state
+	iret
+pa3_push
+	ld a,#2
+	ld ship_state,a
+	iret
 
 
 ;************************************************************************
@@ -144,10 +276,10 @@ at_timerOC_it	DC.W	dummy_rt	; Adresse FFEA-FFEBh
 AVD_it		DC.W	dummy_rt	; Adresse FFEC-FFEDh
 		DC.W	dummy_rt	; Adresse FFEE-FFEFh
 lt_RTC2_it	DC.W	dummy_rt	; Adresse FFF0-FFF1h
-ext3_it		DC.W	dummy_rt	; Adresse FFF2-FFF3h
+ext3_it		DC.W	ship_forward	; Adresse FFF2-FFF3h
 ext2_it		DC.W	dummy_rt	; Adresse FFF4-FFF5h
 ext1_it		DC.W	dummy_rt	; Adresse FFF6-FFF7h
-ext0_it		DC.W	dummy_rt	; Adresse FFF8-FFF9h
+ext0_it		DC.W	ship_backward	; Adresse FFF8-FFF9h
 AWU_it		DC.W	dummy_rt	; Adresse FFFA-FFFBh
 softit		DC.W	dummy_rt	; Adresse FFFC-FFFDh
 reset		DC.W	main		; Adresse FFFE-FFFFh
